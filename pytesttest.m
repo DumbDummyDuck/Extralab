a0704a62c046da1c9c472a7622da55f01687bd4b
DumbDummyDuck <dumbdummyduckspeedhack@rambler.ru>
2022-12-13 22:47:50 +0300

    example & simple generator

---
+++ pytesttest.m
@@ -0,0 +1,11 @@
+% py test
+
+pyversion % did u install Python?
+
+% import test_reg(ex)generation
+% also exec on import
+% if u got stak here try "pip install exrex" in win cmd/powershell !
+py.importlib.import_module('test_reggeneration')
+
+clear ans % then out.txt file 'v been generated, and no more need in it's vars
+%but nevertheless py 'll not be closed until Mlab shutdown
